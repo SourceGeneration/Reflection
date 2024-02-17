1aa0fbd63e63503ce5a86ddecb5b4ba52434ab13
肖昕 <xiao.xin@outlook.com>
2024-02-17 14:51:16 +0100

    Create AutoMapper sample

---
+++ samples/AutoMapper/SourceGeneration.AutoMapper.Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using SourceGeneration.AutoMapper;
+using SourceGeneration.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+ViewModel model = new ViewModel
+{
+    Name = "Hello",
+    Id = 1,
+    Count = 3,
+};
+
+var dto = SourceAutoMapper.Map<ViewModel, ModelDto>(model);
+
+Console.ReadLine();
+
+[SourceReflection]
+public class ViewModel
+{
+    public int Id { get; set; }
+    public int Count { get; set; }
+    public string Name { get; set; }
+}
+
+[SourceReflection]
+public class ModelDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ samples/AutoMapper/SourceGeneration.AutoMapper/SourceAutoMapper.cs
@@ -0,0 +1,31 @@
+﻿using SourceGeneration.Reflection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SourceGeneration.AutoMapper;
+
+public static class SourceAutoMapper
+{
+    [return: NotNullIfNotNull(nameof(source))]
+    public static TTarget? Map<TSource, TTarget>(this TSource source) where TTarget : class, new()
+    {
+        if (source == null)
+            return default;
+
+        var sourceType = SourceReflector.GetType<TSource>();
+        var targetType = SourceReflector.GetType<TTarget>();
+
+        TTarget target = new();
+
+        foreach (var property in sourceType!.GetProperties().Where(x => x.CanRead))
+        {
+            var targetProperty = targetType!.GetProperty(property.Name);
+
+            if (targetProperty != null && targetProperty.CanWrite && !targetProperty.IsInitOnly)
+            {
+                targetProperty.SetValue(target, property.GetValue(source));
+            }
+        }
+
+        return target;
+    }
+}
