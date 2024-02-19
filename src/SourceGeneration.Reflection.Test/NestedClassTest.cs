cdfdc63118119f5ec76db33529b2169715f31633
肖昕 <xiao.xin@outlook.com>
2024-02-19 10:03:20 +0100

    Create NestedType Test

---
+++ src/SourceGeneration.Reflection.Test/NestedClassTest.cs
@@ -0,0 +1,33 @@
+﻿namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class NestedClassTest
+{
+    [TestMethod]
+    public void Public_Nested()
+    {
+        var type = SourceReflector.GetType(typeof(PublicNestedClass));
+        Assert.IsNotNull(type);
+        var instance = type.GetConstructor([])!.Invoke([]);
+    }
+
+    [TestMethod]
+    public void Private_Nested()
+    {
+        var type = SourceReflector.GetType(typeof(PrivateNestedClass));
+        Assert.IsNull(type);
+    }
+
+    [SourceReflection]
+    public class PublicNestedClass
+    {
+        public int Property { get; set; }
+    }
+
+    [SourceReflection]
+    private class PrivateNestedClass
+    {
+        public int Property { get; set; }
+    }
+
+}
