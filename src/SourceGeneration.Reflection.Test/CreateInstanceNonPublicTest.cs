9a0b9d94361d3d9053db7c4826686b5ff2e5a780
肖昕 <xiao.xin@outlook.com>
2024-02-22 08:00:43 +0100

    Create CreateInstance test class

---
+++ src/SourceGeneration.Reflection.Test/CreateInstanceNonPublicTest.cs
@@ -0,0 +1,85 @@
+﻿namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class CreateInstanceNonPublicTest
+{
+    [TestMethod]
+    public void Parameterless()
+    {
+        var instance = (CreateInstanceNonPublicTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceNonPublicTestObject));
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(0L, instance.Long);
+        Assert.AreEqual(null, instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter1()
+    {
+        var instance = (CreateInstanceNonPublicTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceNonPublicTestObject), 1);
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(1, instance.Int);
+        Assert.AreEqual(0L, instance.Long);
+        Assert.AreEqual("b", instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter2()
+    {
+        var instance = (CreateInstanceNonPublicTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceNonPublicTestObject), (byte)1);
+        Assert.AreEqual((byte)1, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(0L, instance.Long);
+        Assert.AreEqual("a", instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter3()
+    {
+        var instance = (CreateInstanceNonPublicTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceNonPublicTestObject), (long)1);
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(1L, instance.Long);
+        Assert.AreEqual("c", instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter_Full()
+    {
+        var instance = (CreateInstanceNonPublicTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceNonPublicTestObject), (long)1, "abc");
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(1L, instance.Long);
+        Assert.AreEqual("abc", instance.String);
+    }
+
+}
+
+[SourceReflection]
+public class CreateInstanceNonPublicTestObject
+{
+    private CreateInstanceNonPublicTestObject() { }
+
+    private CreateInstanceNonPublicTestObject(byte a, string? b = "a")
+    {
+        Byte = a;
+        String = b;
+    }
+
+    private CreateInstanceNonPublicTestObject(int a, string? b = "b")
+    {
+        Int = a;
+        String = b;
+    }
+
+    private CreateInstanceNonPublicTestObject(long a, string? b = "c")
+    {
+        Long = a;
+        String = b;
+    }
+
+    public int Int { get; }
+    public byte Byte { get; }
+    public long Long { get; }
+    public string? String { get; }
+}
---
+++ src/SourceGeneration.Reflection.Test/CreateInstanceTest.cs
@@ -0,0 +1,85 @@
+﻿namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class CreateInstanceTest
+{
+    [TestMethod]
+    public void Parameterless()
+    {
+        var instance = (CreateInstanceTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceTestObject));
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(0L, instance.Long);
+        Assert.AreEqual(null, instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter1()
+    {
+        var instance = (CreateInstanceTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceTestObject), 1);
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(1, instance.Int);
+        Assert.AreEqual(0L, instance.Long);
+        Assert.AreEqual("b", instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter2()
+    {
+        var instance = (CreateInstanceTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceTestObject), (byte)1);
+        Assert.AreEqual((byte)1, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(0L, instance.Long);
+        Assert.AreEqual("a", instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter3()
+    {
+        var instance = (CreateInstanceTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceTestObject), (long)1);
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(1L, instance.Long);
+        Assert.AreEqual("c", instance.String);
+    }
+
+    [TestMethod]
+    public void Parameter_Full()
+    {
+        var instance = (CreateInstanceTestObject)SourceReflector.CreateInstance(typeof(CreateInstanceTestObject), (long)1, "abc");
+        Assert.AreEqual((byte)0, instance.Byte);
+        Assert.AreEqual(0, instance.Int);
+        Assert.AreEqual(1L, instance.Long);
+        Assert.AreEqual("abc", instance.String);
+    }
+
+}
+
+[SourceReflection]
+public class CreateInstanceTestObject
+{
+    public CreateInstanceTestObject() { }
+
+    public CreateInstanceTestObject(byte a, string? b = "a")
+    {
+        Byte = a;
+        String = b;
+    }
+
+    public CreateInstanceTestObject(int a, string? b = "b")
+    {
+        Int = a;
+        String = b;
+    }
+
+    public CreateInstanceTestObject(long a, string? b = "c")
+    {
+        Long = a;
+        String = b;
+    }
+
+    public int Int { get; }
+    public byte Byte { get; }
+    public long Long { get; }
+    public string? String { get; }
+}
