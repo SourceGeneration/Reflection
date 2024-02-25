61e369f03e4459b2c9fe03baddb1d6d54e0cc2a2
肖昕 <xiao.xin@outlook.com>
2024-02-25 13:46:52 +0100

    Update test

--- src/SourceGeneration.Reflection.SourceGenerator.Test/UnitTest1.cs
+++ src/SourceGeneration.Reflection.SourceGenerator.Test/UnitTest1.cs
@@ -8,22 +8,19 @@ namespace SourceGeneration.Reflection.SourceGenerator.Test
         {
             string source = @"
 //[assembly: SourceReflectionAttribute<SourceGeneration.Reflection.Sample2.EnumTestObject>]
-[assembly: SourceReflectionType(typeof(System.Collections.Generic.List<string>))]
+//[assembly: SourceReflectionType(typeof(System.Collections.Generic.List<string>))]
+//[assembly: SourceReflectionType<int[]>]
 using System;
 using System.Threading.Tasks;
 using SourceGeneration.Reflection;
 
 namespace SourceGeneration.Reflection.Sample2
 {
-    //[SourceReflection]
-    //public class IndexTestObject
-    //{
-    //    public string this[int a]
-    //    {
-    //        get => string.Empty;
-    //        set { }
-    //    }
-    //}
+    [SourceReflection]
+    public class IndexTestObject
+    {
+        public IndexTestObject(int* a) { }
+    }
 }
 ";
             var result = CSharpTestGenerator.Generate<ReflectionSourceGenerator>(source, typeof(SourceReflectionAttribute).Assembly);
@@ -31,4 +28,5 @@ namespace SourceGeneration.Reflection.Sample2
             var script2 = result.RunResult.GeneratedTrees.LastOrDefault()?.GetText();
         }
     }
+
 }
