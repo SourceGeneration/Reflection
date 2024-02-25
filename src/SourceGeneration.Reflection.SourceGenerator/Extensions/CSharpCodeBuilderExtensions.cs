fc3a2f9e6764e73a06c322f75b99c134c997e289
肖昕 <xiao.xin@outlook.com>
2024-02-25 05:40:44 +0100

    Initial commit

---
+++ src/SourceGeneration.Reflection.SourceGenerator/Extensions/CSharpCodeBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace System.Text;
+
+internal static class CSharpCodeBuilderExtensions
+{
+    public static void AppendArrayInitializer(this CSharpCodeBuilder builder, string itemType, params string[] items)
+    {
+        if (items == null || items.Length == 0)
+        {
+            builder.Append($"global::System.Array.Empty<{itemType}>()");
+            return;
+        }
+
+        builder.Append($"new {itemType}[] {{ ");
+        for (int i = 0; i < items.Length; i++)
+        {
+            builder.Append(items[i]);
+            if (i < items.Length - 1)
+            {
+                builder.Append(", ");
+            }
+        }
+        builder.Append(" }");
+    }
+}
