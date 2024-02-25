f209669022523b7a54bcd824bc6bb88a610d5ce2
肖昕 <xiao.xin@outlook.com>
2024-02-25 05:40:58 +0100

    Initial commit

---
+++ src/SourceGeneration.Reflection.Test/AssemblyAttributeTest.cs
@@ -0,0 +1,19 @@
+﻿using SourceGeneration.Reflection;
+
+[assembly: SourceReflectionType<object>]
+[assembly: SourceReflectionType<Array>]
+[assembly: SourceReflectionType(typeof(Enumerable))]
+
+namespace SourceGeneration.Reflection.Test;
+
+
+[TestClass]
+public class AssemblyAttributeTest
+{
+    [TestMethod]
+    public void Object()
+    {
+        var type = SourceReflector.GetRequiredType<object>();
+
+    }
+}
