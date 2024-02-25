cebc77ac8269788e24b1bc928e2c3baa9ce53e62
肖昕 <xiao.xin@outlook.com>
2024-02-25 06:27:34 +0100

    Update default value

--- src/SourceGeneration.Reflection/SourceMethodBase.cs
+++ src/SourceGeneration.Reflection/SourceMethodBase.cs
@@ -4,5 +4,5 @@ public abstract class SourceMethodBase : SourceMemberInfo
 {
     public SourceAccessibility Accessibility { get; init; }
     public bool IsStatic { get; init; }
-    public SourceParameterInfo[] Parameters { get; init; } = default!;
+    public SourceParameterInfo[] Parameters { get; init; } = [];
 }
