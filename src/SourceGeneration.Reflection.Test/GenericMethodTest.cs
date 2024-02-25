c2d2949683cc12f87c304c2a9d72af15e4ac884c
肖昕 <xiao.xin@outlook.com>
2024-02-25 07:30:30 +0100

    Update test

--- src/SourceGeneration.Reflection.Test/GenericMethodTest.cs
+++ src/SourceGeneration.Reflection.Test/GenericMethodTest.cs
@@ -99,26 +99,51 @@ public class GenericMethodTest
 
         Assert.AreEqual("a", method.Invoke(instance, ["a", "b"]));
     }
+
+    [TestMethod]
+    public void TypeParameterArray()
+    {
+        SourceTypeInfo type = SourceReflector.GetRequiredType<GenericMethodTestObject>();
+
+        var method = type.GetMethod("InvokeArray1")!;
+
+        GenericMethodTestObject instance = new();
+
+        Assert.ThrowsException<InvalidOperationException>(() =>
+        {
+            method.Invoke(instance, [new string[] { "a", "b" }]);
+        });
+    }
+
+    [TestMethod]
+    public void TestInfer()
+    {
+        var type = SourceReflector.GetRequiredType<GenericMethodTestInferObject>();
+        GenericMethodTestInferObject instance = new();
+
+        Assert.AreEqual("Object", type.GetMethod("Invoke1")!.Invoke(instance, [1]));
+        Assert.AreEqual("ICloneable", type.GetMethod("Invoke2")!.Invoke(instance, ["a"]));
+
+    }
 }
 
 [SourceReflection]
 public class GenericMethodTestObject
 {
     public T Invoke0<T>() => default!;
-
     public T Invoke1<T>(T t) => t;
-
     public T Invoke2<T>(T t) where T : ICloneable => t;
-
     public T Invoke3<T>(T t) where T : unmanaged => t;
-
     public T Invoke4<T>(T t) where T : notnull => t;
-
     public T Invoke5<T>(T t) where T : ICloneable, IComparable => t;
-
     public T Invoke6<T, K>(T t, K k) where T : ICloneable where K : IComparable => t;
-
-
     public T[] InvokeArray1<T>(T[] t) => t;
+}
 
+
+[SourceReflection]
+public class GenericMethodTestInferObject
+{
+    public string Invoke1<T>(T value) => typeof(T).Name;
+    public string Invoke2<T>(T value) where T : ICloneable => typeof(T).Name;
 }
--- src/SourceGeneration.Reflection.Test/IndexerTest.cs
+++ src/SourceGeneration.Reflection.Test/IndexerTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SourceGeneration.Reflection.Test;
+﻿namespace SourceGeneration.Reflection.Test;
 
 [TestClass]
 public class IndexerTest
