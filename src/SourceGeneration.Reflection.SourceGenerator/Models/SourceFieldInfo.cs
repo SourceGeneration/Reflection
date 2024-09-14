3f32a05bb71c836391eaeacd110cfaf41b8a3a3b
肖昕 <xiao.xin@outlook.com>
2024-09-14 15:55:38 +0800

    Add IsGenericDictionaryType, IsGenericEnumerableType property

--- src/SourceGeneration.Reflection.SourceGenerator/Models/SourceFieldInfo.cs
+++ src/SourceGeneration.Reflection.SourceGenerator/Models/SourceFieldInfo.cs
@@ -6,6 +6,8 @@ internal class SourceFieldInfo
 {
     public string Name;
     public string FieldType;
+    public bool IsGenericDictionaryType;
+    public bool IsGenericEnumerableType;
     public bool IsRequired;
     public bool IsStatic;
     public bool IsReadOnly;
