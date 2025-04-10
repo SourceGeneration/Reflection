e69736c5e9ed3521f497c1b9df1e8f76b8f6602b
肖昕 <xiao.xin@outlook.com>
2025-04-10 16:43:39 +0800

    Add initializer test

---
+++ src/SourceGeneration.Reflection.Test/InitializerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class InitializerTest
+{
+    [TestMethod]
+    public void Test()
+    {
+        Stopwatch sw = new();
+        sw.Start();
+
+        __SourceReflectorInitializer.Initialize();
+        sw.Stop();
+    }
+}
