3be45b10d6c92b0c5942784d750e47657d5c4c19
肖昕 <xiao.xin@outlook.com>
2025-04-11 15:09:45 +0800

    add inherited test

---
+++ src/SourceGeneration.Reflection.Test/InheritedTest.cs
@@ -0,0 +1,21 @@
+﻿namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class InheritedTest
+{
+    [TestMethod]
+    public void Inherited()
+    {
+        var type1 = SourceReflector.GetType<InheritedObject1>();
+        var type2 = SourceReflector.GetType<InheritedObject2>();
+        var baseType = SourceReflector.GetType<BaseObject>();
+
+        Assert.IsNotNull(type1);
+        Assert.IsNotNull(type2);
+        Assert.IsNotNull(baseType);
+    }
+}
+
+public class BaseObject { }
+[SourceReflection] public class InheritedObject1 : BaseObject { }
+[SourceReflection] public class InheritedObject2 : BaseObject { }
