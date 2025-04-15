61a655bec55dc894be0afa29d19d769d018ab5d9
肖昕 <xiao.xin@outlook.com>
2025-04-15 12:00:25 +0800

    initialize commit

---
+++ src/SourceGeneration.Reflection.Test/GenericDictionaryTypeMemberTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class GenericDictionaryTypeMemberTest
+{
+    [TestMethod]
+    public void GenericDictionary()
+    {
+        var typeInfo = SourceReflector.GetRequiredType<GenericDictionaryMemberType>();
+
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.Int))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.Object))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.NonGenericDictionaryInterface))!.IsGenericDictionaryType);
+
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.Dictionary))!.IsGenericDictionaryType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.SortedDictionary))!.IsGenericDictionaryType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.DictionaryInterface))!.IsGenericDictionaryType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.CustomDictionary))!.IsGenericDictionaryType);
+
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.Dictionary))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.SortedDictionary))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.DictionaryInterface))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericDictionaryMemberType.CustomDictionary))!.IsGenericEnumerableType);
+    }
+}
+
+[SourceReflection]
+public class GenericDictionaryMemberType
+{
+    public object? Object { get; set; }
+    public int Int { get; set; }
+    public SortedDictionary<string, string>? SortedDictionary { get; set; }
+    public Dictionary<string, string>? Dictionary { get; set; }
+    public IDictionary<string, string>? DictionaryInterface { get; set; }
+    public IDictionary? NonGenericDictionaryInterface { get; set; }
+
+    public CustomDictionary? CustomDictionary;
+}
+
+public class CustomDictionary : IDictionary<int, object>
+{
+    public object this[int key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public ICollection<int> Keys => throw new NotImplementedException();
+
+    public ICollection<object> Values => throw new NotImplementedException();
+
+    public int Count => throw new NotImplementedException();
+
+    public bool IsReadOnly => throw new NotImplementedException();
+
+    public void Add(int key, object value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(KeyValuePair<int, object> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(KeyValuePair<int, object> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ContainsKey(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(KeyValuePair<int, object>[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<KeyValuePair<int, object>> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(KeyValuePair<int, object> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool TryGetValue(int key, [MaybeNullWhen(false)] out object value)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ src/SourceGeneration.Reflection.Test/GenericEnumerableTypeMemberTest.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace SourceGeneration.Reflection.Test;
+
+[TestClass]
+public class GenericEnumerableTypeMemberTest
+{
+    [TestMethod]
+    public void GenericEnumerable()
+    {
+        var typeInfo = SourceReflector.GetRequiredType<GenericEnumerableMemberType>();
+
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.Int))!.IsGenericEnumerableType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.Object))!.IsGenericEnumerableType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.NonGenericEnumerableInterface))!.IsGenericEnumerableType);
+
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.EnumerableInterface))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.ListInterface))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.List))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.ReadOnlyCollection))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.CustomList))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.CustomCollection))!.IsGenericEnumerableType);
+        Assert.IsTrue(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.CustomEnumerable))!.IsGenericEnumerableType);
+
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.EnumerableInterface))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.ListInterface))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.List))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.ReadOnlyCollection))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.CustomList))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.CustomCollection))!.IsGenericDictionaryType);
+        Assert.IsFalse(typeInfo.GetFieldOrProperty(nameof(GenericEnumerableMemberType.CustomEnumerable))!.IsGenericDictionaryType);
+    }
+}
+
+[SourceReflection]
+public class GenericEnumerableMemberType
+{
+    public object? Object { get; set; }
+    public int Int { get; set; }
+    public IEnumerable<string>? EnumerableInterface { get; set; }
+    public IList<string>? ListInterface { get; set; }
+    public List<string>? List { get; set; }
+    public ReadOnlyCollection<string>? ReadOnlyCollection { get; set; }
+    public IEnumerable? NonGenericEnumerableInterface { get; set; }
+
+    public CustomList? CustomList;
+    public CustomCollection? CustomCollection;
+    public CustomEnumerable? CustomEnumerable;
+}
+
+public class CustomList : CustomCollection, IList<int>
+{
+    public int this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public int IndexOf(int item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(int index, int item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CustomCollection : ICollection<int>
+{
+    public int Count => throw new NotImplementedException();
+
+    public bool IsReadOnly => throw new NotImplementedException();
+
+    public void Add(int item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(int item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(int[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(int item)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+public class CustomEnumerable : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
