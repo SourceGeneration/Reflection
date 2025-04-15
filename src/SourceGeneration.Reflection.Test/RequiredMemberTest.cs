cc791d3d2773d6c2bf4fd080eef57af048ddfe96
肖昕 <xiao.xin@outlook.com>
2025-04-15 11:44:17 +0800

    add base type required member test

--- src/SourceGeneration.Reflection.Test/RequiredMemberTest.cs
+++ src/SourceGeneration.Reflection.Test/RequiredMemberTest.cs
@@ -1,38 +1,120 @@
-﻿namespace SourceGeneration.Reflection.Test;
+﻿using SourceGeneration.Reflection.SourceGenerator.TestLib;
+
+namespace SourceGeneration.Reflection.Test;
 
 [TestClass]
 public class RequiredMemberTest
 {
     [TestMethod]
-    public void GetInfo()
+    public void GetRequiredPropertyInfo()
     {
-        var type = SourceReflector.GetType(typeof(RequiredMemberTestObject));
+        var type = SourceReflector.GetType(typeof(RequiredPropertyTestObject));
         Assert.IsNotNull(type);
-        var p = type.GetProperty("Property");
-        var f = type.GetField("Field");
-        Assert.IsNotNull(p);
-        Assert.IsNotNull(f);
 
+        var p = type.GetProperty("A");
+        Assert.IsNotNull(p);
         Assert.IsTrue(p.IsRequired);
+    }
+
+    [TestMethod]
+    public void GetRequiredFieldInfo()
+    {
+        var type = SourceReflector.GetType(typeof(RequiredFieldTestObject));
+        Assert.IsNotNull(type);
+
+        var f = type.GetField("A");
+        Assert.IsNotNull(f);
         Assert.IsTrue(f.IsRequired);
     }
 
     [TestMethod]
-    public void Construct()
+    public void ConstructRequiredPropertyClass()
     {
-        var type = SourceReflector.GetType(typeof(RequiredMemberTestObject));
+        var type = SourceReflector.GetType(typeof(RequiredPropertyTestObject));
         Assert.IsNotNull(type);
-        var instance = (RequiredMemberTestObject)type.GetConstructor([])!.Invoke([]);
+        var instance = (RequiredPropertyTestObject)type.GetConstructor([])!.Invoke([]);
         Assert.IsNotNull(instance);
 
-        Assert.AreEqual(1, instance.Field);
-        Assert.AreEqual(1, instance.Property);
+        Assert.AreEqual(1, instance.A);
+    }
+
+    [TestMethod]
+    public void ConstructRequiredFieldClass()
+    {
+        var type = SourceReflector.GetType(typeof(RequiredFieldTestObject));
+        Assert.IsNotNull(type);
+        var instance = (RequiredFieldTestObject)type.GetConstructor([])!.Invoke([]);
+        Assert.IsNotNull(instance);
+        Assert.AreEqual(1, instance.A);
+    }
+
+    [TestMethod]
+    public void ConstructDerivedType()
+    {
+        var type = SourceReflector.GetType(typeof(RequiredFieldTestObject));
+        Assert.IsNotNull(type);
+        var instance = (RequiredFieldTestObject)type.GetConstructor([])!.Invoke([]);
+        Assert.IsNotNull(instance);
+        Assert.AreEqual(1, instance.A);
+    }
+
+    [TestMethod]
+    public void ConstructRequiredMemberFromLibClass()
+    {
+        var type = SourceReflector.GetType(typeof(RequiredMemberDerivedLibType));
+        Assert.IsNotNull(type);
+        var instance = (RequiredMemberDerivedLibType)type.GetConstructor([])!.Invoke([]);
+        Assert.IsNotNull(instance);
+        Assert.AreEqual(1, instance.RequiredProperty);
     }
 }
 
 [SourceReflection]
-public class RequiredMemberTestObject
+public class RequiredPropertyTestObject
 {
-    public required int Property { get; set; } = 1;
-    public required int Field = 1;
+    public required int A { get; set; } = 1;
+}
+
+[SourceReflection]
+public class RequiredFieldTestObject
+{
+    public required int A = 1;
+}
+
+[SourceReflection]
+public class RequiredMemberBaseType
+{
+    public required int A { get; set; } = 1;
+    public required int B = 1;
+}
+
+[SourceReflection]
+public class RequiredMemberDerivedType1 : RequiredMemberBaseType
+{
+    public required int C = 1;
+}
+
+[SourceReflection]
+public class RequiredMemberDerivedType2 : RequiredMemberDerivedType1
+{
+}
+
+[SourceReflection]
+public class RequiredMemberDerivedLibType : LibTestClass
+{
+}
+
+
+[SourceReflection]
+public class RequiredMemberDefaultValueType
+{
+    public required int A { get; set; } = 1;
+    public required bool B = true;
+    public required string C { get; set; } = "abced";
+    public required DateTime D { get; set; } = new DateTime(1234567);
+    public required TimeOnly E { get; set; } = TimeOnly.MaxValue;
+    public required object F = new();
+    public required List<string> H { get; set; } = [];
+    //public required RequiredMemberDerivedType1 G { get; set; } = new RequiredMemberDerivedType1 { A = 1, B = 2, C = 3 };
+
 }
